52aae77056420ab0f2376f092cf687fdedb26d0a
Aparna garg <aparnagargstg04@gmail.com>
2023-05-05 21:30:41 +0530

    robofriends

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './robots';
+import search from './SearchBox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
